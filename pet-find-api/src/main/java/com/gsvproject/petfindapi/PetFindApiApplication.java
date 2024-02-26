5b339ee31d6a046cccf5bdda17853154d20b9756
Vilson Souza <vilsonsouza758@gmail.com>
2024-02-26 08:49:09 -0300

    init

---
+++ pet-find-api/src/main/java/com/gsvproject/petfindapi/PetFindApiApplication.java
@@ -0,0 +1,13 @@
+package com.gsvproject.petfindapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFindApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFindApiApplication.class, args);
+	}
+
+}
