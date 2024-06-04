4aea2d5a42311349e44642d2fd50108ed3613a29
vilson souza <vilsonsouza758@gmail.com>
2024-06-04 17:15:25 -0300

    Swagger Config/ H2 Database

--- petFindApi/src/main/java/com/gsv/petFindApi/PetFindApiApplication.java
+++ petFindApi/src/main/java/com/gsv/petFindApi/PetFindApiApplication.java
@@ -1,4 +1,4 @@
-package com.gsvproject.petfindapi;
+package com.gsv.petFindApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- petFindApi/src/test/java/com/gsv/petFindApi/PetFindApiApplicationTests.java
+++ petFindApi/src/test/java/com/gsv/petFindApi/PetFindApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gsvproject.petfindapi;
+package com.gsv.petFindApi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
